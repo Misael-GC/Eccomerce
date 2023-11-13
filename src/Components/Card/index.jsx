ec15963f3ef3dc9c42fde4562624fc81a0c9ca5f
Misael-GC <misael.gomez1623@gmail.com>
2023-11-13 16:50:04 -0600

    Maquetando el ProductDetail, abriendo y cerrando el modal

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -5,7 +5,11 @@ import { ShoppingCartContext }  from '../../Context'
 function Card(data) {
   const context = useContext(ShoppingCartContext);
   return (
-    <div className='bg-white cursor-pointer mb-15 w-56 h-60 rounded-lg '>
+    
+    <div
+      className='bg-white cursor-pointer mb-15 w-56 h-60 rounded-lg '
+      onClick={()=> context.openProductDetail()}
+      >
         <figure className='relative mb-2 w-full h-4/5'>
             <span className='absolute bottom-0 left-0 bg-white/80 rounded-lg text-black text-xs m-2 px-3 py-0.5 '>{data.data?.category}</span>
             <img 
--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -1,16 +1,21 @@
 import { XCircleIcon  } from '@heroicons/react/24/solid'
 import './details.css';
-
+import { useContext } from 'react';
+import { ShoppingCartContext }  from '../../Context'
 
 
 function ProductDetail() {
+  const context = useContext(ShoppingCartContext);
   return (
-    <aside className="product-detail flex flex-col fixed right-0 border border-black rounded-lg bg-white">
+    <aside className={`${context.isProductDetailOpen ? 'flex' : 'hidden'} product-detail flex-col fixed right-0 border border-black rounded-lg bg-white`}>
       <div className='flex justify-between items-center p-7'>
       <h2 className="text-xl font-medium">Product Detail</h2>
-      <div>
-        <XCircleIcon className="h-5 w-5 text-black"/>
-      </div>
+      <button>
+        <XCircleIcon 
+        className={`h-5 w-5 text-black`}
+          onClick={()=> context.closeProductDetail()}
+        />
+      </button>
       </div>
     </aside>
   );
