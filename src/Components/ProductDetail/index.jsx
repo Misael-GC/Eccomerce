17822b23c546c52eea4646066420213db82c1e2a
Misael-GC <misael.gomez1623@gmail.com>
2023-11-13 15:05:25 -0600

    creando el componente y agregandolo a la interfaz: detalles del producto

---
+++ src/Components/ProductDetail/index.jsx
@@ -0,0 +1,15 @@
+import './details.css';
+
+
+function ProductDetail() {
+  return (
+    <aside className="product-detail flex flex-col fixed right-0 border border-black rounded-lg bg-white">
+      <div className='flex justify-between items-center p-7'>
+      <h2 className="text-xl font-medium">Product Detail</h2>
+      <div>x</div>
+      </div>
+    </aside>
+  );
+}
+
+export default ProductDetail;
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import Layout from '../../Components/Layout';
 import Card from '../../Components/Card';
+import ProductDetail from '../../Components/ProductDetail';
 import { apiUrl } from '../../api/';
 
 function Home() {
@@ -27,6 +28,7 @@ function Home() {
         items && items.map(item => <Card key={item.id} data={item} />)
       }
       </div>
+      <ProductDetail />
     </Layout>
   )
 }
