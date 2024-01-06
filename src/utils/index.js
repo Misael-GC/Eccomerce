36345f41d06a70e5d89d51ad08de667c0010f6a7
Misael-GC <misael.gomez1623@gmail.com>
2024-01-06 16:08:57 -0600

    Suma total de productos en el carrito, alternativa del calculo

--- src/utils/index.js
+++ src/utils/index.js
@@ -4,7 +4,8 @@
 *@returns {number} Total price
 */
 export const totalPrice = (products) => {
-   let sum = 0;
-   products.forEach( product => sum += product.price)
-   return sum
+   //let sum = 0;
+   //products.forEach( product => sum += product.price)
+   //return sum
+   return products.reduce((sum, product) => sum + product.price, 0) //alternativa
 }
