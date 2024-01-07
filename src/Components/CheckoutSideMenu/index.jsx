3c5157629dea58c45c2f28039a35feb71571d288
Misael-GC <misael.gomez1623@gmail.com>
2024-01-06 23:28:32 -0600

    Flujo para crear una nueva orden, se creo un useState, y los datos para checkout

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -12,7 +12,18 @@ function CheckoutSideMenu() {
     const filteredProducts = context.cartProducts.filter(product => product.id != id)
     context.setCartProducts(filteredProducts)
   }
-  
+
+  const handleCheckout = () =>{
+    const orderToAdd = {
+      date: '01.02.2024',
+      products: context.cartProducts,
+      totalProducts: context.cartProducts.length,
+      totalPrice: totalPrice(context.cartProducts),
+    }
+
+    context.setOrder([...context.order, orderToAdd])
+    context.setCartProducts([])
+  }
 
   return (
     <aside className={`${context.isCheckoutSideMenuOpen ? 'flex' : 'hidden'} checkout-side-menu flex-col fixed right-0 border border-black rounded-lg bg-white z-20`}>
@@ -25,7 +36,7 @@ function CheckoutSideMenu() {
         />
       </button>
       </div>
-      <div className='px-6 overflow-y-scroll'>
+      <div className='px-6 overflow-y-scroll flex-1'>
       {
         context.cartProducts.map(product =>(
           <OrderCard
@@ -39,11 +50,12 @@ function CheckoutSideMenu() {
         ))
       }
       </div>
-      <div className="px-6">
-        <p className='flex justify-between items-center' >
+      <div className="px-6 mb-7">
+        <p className='flex justify-between items-center mb-2' >
         <span className='font-light'>Total: </span>
         <span className='font-medium text-2xl'>${totalPrice(context.cartProducts)}</span>
         </p>
+        <button className='bg-black py-3 text-white  w-full rounded-lg' onClick={()=> handleCheckout()}>Checkout</button>
       </div>
     </aside>
   );
--- src/utils/index.js
+++ src/utils/index.js
@@ -3,6 +3,7 @@
 *@param {Array} products cartProduct: Array of Objects
 *@returns {number} Total price
 */
+
 export const totalPrice = (products) => {
    //let sum = 0;
    //products.forEach( product => sum += product.price)
